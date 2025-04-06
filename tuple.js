bc23962d9025cedff2e45b133032dfea9668d442
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-06 21:43:54 +0900

    feat: 튜플 예제 추가

---
+++ tuple.js
@@ -0,0 +1,5 @@
+"use strict";
+let greeting = [1, "hello", true];
+for (let i = 0; i < greeting.length; i++) {
+    console.log(greeting[i]);
+}
---
+++ tuple.ts
@@ -0,0 +1,5 @@
+let greeting: [number, string, boolean] = [1, "hello", true];
+
+for (let i = 0; i < greeting.length; i++) {
+    console.log(greeting[i]);
+}
